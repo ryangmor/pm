f7c5ca125a01d6a1030415d879e11fd4106121f0
ryangmor <ryangmor@gmail.com>
2016-03-31 18:41:12 +0430

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,5 +1,5 @@
 return {
-	bot_api_key = '204669943:AAFaXSvZtL_tUfBX-gX3-dhA995o4BpzqNI', --token
+	bot_api_key = '201987057:AAHC8i8OI-0EfCc4bt_lFvU3Gk0edypRtLA', --token
 	bot_api_key = '',
 	google_api_key = '',
 	google_cse_key = '',
@@ -15,8 +15,8 @@ return {
 	lang = 'en',
 	-- If you change this, make sure you also modify launch-tg.sh.
 	cli_port = 4567,
-	admin = 102490691,
-	admin_name = 'John Smith',
+	admin = 52038630,
+	admin_name = 'Ryan Gmor',
 	log_chat = nil,
 	about_text = [[
 
@@ -57,7 +57,7 @@ return {
 	},
 	moderation = {
 		admins = {
-			['102490691'] = 'Arman'
+			['52038630'] = 'RyanGmor'
 		},
 		errors = {
 			antisquig = 'This group is English-only.',
